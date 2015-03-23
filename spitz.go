b0e37d7e6e69677ee9778a1759c0f87e78b1b6db
Patrick Crosby <pcrosby@gmail.com>
2015-03-22 21:33:19 -0500

    import path

--- spitz.go
+++ spitz.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an MIT-style license
 // that can be found in the LICENSE file.
 
-package spitz
+package spitz // import "stathat.com/c/spitz"
 
 import (
 	"bytes"
